ffd69f4fb415dfecd74130dba3b2874b7435cc7b
Wyatt Queirolo <wfq2@cornell.edu>
2017-04-15 14:18:54 -0400

    changed fare file

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
+var planeFare,pubTransport,taxiPrices,mealCosts;
+
 d3.queue()
     .defer(d3.csv,'AverageFare.csv')
     .defer(d3.csv,'publicTransportPrices.csv')
     .defer(d3.csv,'taxiPrices.csv')
     .defer(d3.csv,'avg_meal_cost.csv')
     .await( function (error,planeFare,pubTransport,taxiPrices,mealCosts) {
-
+    	planeFare = planeFare;
+    	pubTransport = pubTransport;
+    	taxiPrices = taxiPrices;
+    	mealCosts = mealCosts;
     });
