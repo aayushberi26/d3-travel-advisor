daf40da356756e44d029b553b1473a6a6228ca78
Aayush Beri <aayushberi@dhcp-hol-3484.eduroam.cornell.edu>
2017-04-15 14:39:27 -0400

    loaded data

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
-var planeFare;
-var pubTransport;
-var taxiPrices;
-var mealCosts;
-var lodgingData;
-var locationData;
+var aggregateData;
+var cityData = [];
 
 d3.queue()
-    .defer(d3.csv, )
-    .await( function (error, planeRaw, publicTransportRaw, taxiRaw, mealRaw, lodgingRaw, locationRaw, aggregateData) {
-        planeFare = planeRaw;
-        pubTransport = publicTransportRaw;
-        taxiPrices = taxiRaw;
-        mealCosts = mealRaw;
-        lodgingData = lodgingRaw;
-        locationData = locationRaw;
+    .defer(d3.csv, "aggregateData.csv")
+    .await( function (error, rawData) {
+        aggregateData = rawData;
+        aggregateData.forEach(function (d) {
+            var city = {
+                city: d["City"],
+                airbnb: Number(d["Airbnb"]),
+                hotel: Number(d["Hotel"]),
+                latitude: Number(d["Latitude"]),
+                longitude: Number(d["Longitude"]),
+                expensiveMeal: Number(d["Meal for 2 People, Mid-range Restaurant, Three-course"])/ 2, // divide by two to get data for 1 person
+                cheapMeal: Number(d["Meal, Inexpensive Restaurant"]),
+                publicTransport: Number(d["Public Transport One Way"]),
+                taxiCost: Number(d["Taxi Base"]) + 10*(Number(d["Taxi 1km"])/1.609), // convert from km to miles for 10 miles
+            }
+            cityData.push(city);
+        });
     });
