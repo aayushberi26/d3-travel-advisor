3c8112fb99ed2a24e35279ee079d320aee1591a3
Jeannie Fu <jf586@cornell.edu>
2017-04-17 18:35:50 -0400

    Adding US map svg

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 var aggregateData;
+var stateMapData;
 var cityData = [];
 
+var projection = d3.geoAlbersUsa().scale(75);
+var pathGenerator = d3.geoPath().projection(projection);
+
 d3.queue()
+    .defer(d3.json, "us.json")
     .defer(d3.csv, "aggregateData.csv")
-    .await( function (error, rawData) {
+    .await( function (error, rawMap, rawData) {
+        stateMapData = topojson.feature(rawMap, rawMap.objects.states);
+
         aggregateData = rawData;
         aggregateData.forEach(function (d) {
             var city = {
@@ -19,4 +26,21 @@ d3.queue()
             }
             cityData.push(city);
         });
+
+        var svg = d3.select("#svgMap"); // We can rename these
+        showMap(svg) // might want to pass in more?
     });
+
+function showMap(svg) {
+    console.log("Showing map");
+    projection.fitExtent([[0,0], [svg.attr("width"), svg.attr("height")]], stateMapData);
+    pathGenerator = d3.geoPath().projection(projection);
+
+    var paths = svg.selectAll("path.state").data(stateMapData.features);
+    paths.enter().append("path").attr("class", "state")
+    .merge(paths)
+    .style("fill", "#aaa")
+    .attr("d", function (state) {
+        return pathGenerator(state);
+    });
+}
